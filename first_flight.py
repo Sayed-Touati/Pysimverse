f6c35fdfc06fe5c9434c6f0564bc4b6455fd5d43
Sayed Touati <sayedtouati@outlook.com>
2026-04-08 00:26:21 +0100

    add basic drone takeoff and landing

--- first_flight.py
+++ first_flight.py
@@ -1,8 +1,15 @@
-# Importing the Drone class form pysimverse
+# Importing the Drone class from pysimverse
 from pysimverse import Drone
+import time
 
 # Create a drone object
 drone = Drone()
 
 # Connect to the drone
 drone.connect()
+
+drone.take_off()
+
+time.sleep(6) # wait 3 sec while the drone stays airborne
+drone.land()
+time.sleep(4)
