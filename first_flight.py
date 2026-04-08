722bce847cecb9f613c58a10929bc496bcccd6e3
Sayed Touati <sayedtouati@outlook.com>
2026-04-08 01:09:18 +0100

    add drone movement and rotation

--- first_flight.py
+++ first_flight.py
@@ -8,8 +8,28 @@ drone = Drone()
 # Connect to the drone
 drone.connect()
 
-drone.take_off()
+drone.take_off() # Take off to the default height, which is often 100 cm in this package
 
-time.sleep(6) # wait 3 sec while the drone stays airborne
+"""
+drone.move_down(20)
+time.sleep(2)
+drone.move_up(30)
+time.sleep(2)
+drone.move_left(20)
+time.sleep(2)
+drone.move_right(20)
+time.sleep(2)
+drone.move_forward(90)
+time.sleep(2)
+drone.move_backward(40)
+time.sleep(2)
+"""
+
+drone.set_speed(50)
+drone.move_forward(150)
+
+# drone.rotate(-45)
+# time.sleep(1)
+
 drone.land()
 time.sleep(4)
